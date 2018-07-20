5d5cff60344d3ae943852ac83bd24af8309e018e
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-07-20 18:43:08 +0800

    no message

--- NeoKB_try/RulesetMeteor/Config/MeteorConfigManager.cpp
+++ NeoKB_try/RulesetMeteor/Config/MeteorConfigManager.cpp
@@ -19,7 +19,7 @@ int MeteorConfigManager::InitializeDefault()
 	Set(MeteorSetting::FallSpeed, MTO_FLOAT(16.f));
 	Set(MeteorSetting::FallBrightness, MTO_FLOAT(1.f));
 	Set(MeteorSetting::ExplodeSpeed, MTO_FLOAT(16.f));
-	Set(MeteorSetting::FallBrightness, MTO_FLOAT(1.f));
+	Set(MeteorSetting::ExplodeBrightness, MTO_FLOAT(1.f));
 	Set(MeteorSetting::GlowLineSpeed, MTO_FLOAT(1.f));
 	Set(MeteorSetting::GlowLineBrightness, MTO_FLOAT(1.f));
 	Set(MeteorSetting::GlowLineDuration, MTO_FLOAT(1.f));
