686f42152644030b2156a5c2fa531946d25f1667
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-07-20 19:49:26 +0800

    no message

--- NeoKB_try/Base/Sheetmusic/SheetmusicPostprocessor.cpp
+++ NeoKB_try/Base/Sheetmusic/SheetmusicPostprocessor.cpp
@@ -12,7 +12,10 @@ Sm<Event>* SmPostprocessor::postprocess(Sm<Event>* s)
 {
 	LOG(LogLevel::Fine) << "Sm<Event>* SmPostprocessor::postprocess(Sm<Event>*) : Start sorting ...";
 
-	sort(s->GetEvents()->begin(), s->GetEvents()->end());
+	sort(s->GetEvents()->begin(), s->GetEvents()->end(),
+		[](Event* const& a, Event* const& b) { return a->GetStartTime() < b->GetStartTime(); });
+
+	// sort(s->GetEvents()->begin(), s->GetEvents()->end());
 	return s;
 }
 
