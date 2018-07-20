7123a8afa9bed7ec1f802b35f6a47a48c8be1370
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-07-20 19:44:05 +0800

    no message

--- NeoKB_try/Base/Sheetmusic/SheetmusicPostprocessor.cpp
+++ NeoKB_try/Base/Sheetmusic/SheetmusicPostprocessor.cpp
@@ -10,6 +10,8 @@ using namespace Base::Sheetmusics;
 
 Sm<Event>* SmPostprocessor::postprocess(Sm<Event>* s)
 {
+	LOG(LogLevel::Fine) << "Sm<Event>* SmPostprocessor::postprocess(Sm<Event>*) : Start sorting ...";
+
 	sort(s->GetEvents()->begin(), s->GetEvents()->end());
 	return s;
 }
