1ad6ea243bfdc5cd629ffca974fc1329860e04b8
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-09-14 03:24:17 +0800

    no message

--- NeoKB_try/Base/Scheduler/Scheduler.cpp
+++ NeoKB_try/Base/Scheduler/Scheduler.cpp
@@ -84,7 +84,7 @@ int Scheduler::Elapse(MTO_FLOAT elapsedTime) {
 
 	while (eventProcessors->front()->GetStartTime() < currentTime) {
 
-		if (eventProcessors->front()->GetStartTime() < 0) {
+		if (eventProcessors->front()->GetStartTime() <= 0) {
 			LOG(LogLevel::Finer) << "Scheduler::Elapse() : event [" << eventProcessors->back()->GetStartTime() << "] is less than 0. Killed directly";
 
 			eventProcessors->erase(eventProcessors->begin());
