94a731502628fca323e160158dee3ec31baf33da
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-09-13 17:09:34 +0800

    no message

--- NeoKB_try/Program.cpp
+++ NeoKB_try/Program.cpp
@@ -32,35 +32,43 @@ int main(int argc, char *argv[]) {
 	LOG(LogLevel::Info) << "int main(int,char*) : Create game [MeteorGame].";
 	Game* game = new MeteorGame();
 
+
 	LOG(LogLevel::Fine) << "int main(int,char*) : Create Config Manager [FrameworkConfigManager].";
 	FrameworkConfigManager* fConfigManager = new FrameworkConfigManager();
 	fConfigManager->Async();
 	/**********/
 	aplayCommand = string("aplay Resources/Sms/") + argv[1] + string(".wav");			// 這行之後要刪掉
-	fConfigManager->Set(FrameworkSetting::SongTitle, string(argv[1]));  // 這行之後要刪掉
+	fConfigManager->Set(FrameworkSetting::SongTitle				, string(argv[1]));  // 這行之後要刪掉
 	/**********/
-	fConfigManager->Set(FrameworkSetting::PatternGenerator, string("MeteorPatternGenerator"));
-	fConfigManager->Set(FrameworkSetting::HardwareVersion, 10);
-	fConfigManager->Set(FrameworkSetting::Width, 48); //要改
-	fConfigManager->Set(FrameworkSetting::Height, 24);
-	fConfigManager->Set(FrameworkSetting::BlackKeyHeight, 16);
-	fConfigManager->Set(FrameworkSetting::TargetHeight, 20);
-	fConfigManager->Set(FrameworkSetting::BlackKeyTargetHeight, 14);
-	fConfigManager->Set(FrameworkSetting::StartPitch, 24);
-	fConfigManager->Set(FrameworkSetting::FrameRate, 30);
-	// TODO: 設定....
+	fConfigManager->Set(FrameworkSetting::PatternGenerator		, string("MeteorPatternGenerator"));
+	fConfigManager->Set(FrameworkSetting::HardwareVersion		, 10);
+	fConfigManager->Set(FrameworkSetting::Width					, 48); //要改
+	fConfigManager->Set(FrameworkSetting::Height				, 24);
+	fConfigManager->Set(FrameworkSetting::BlackKeyHeight		, 16);
+	fConfigManager->Set(FrameworkSetting::TargetHeight			, 20);
+	fConfigManager->Set(FrameworkSetting::BlackKeyTargetHeight	, 14);
+	fConfigManager->Set(FrameworkSetting::StartPitch			, 24);
+	fConfigManager->Set(FrameworkSetting::FrameRate				, 30);
 	game->Cache<FrameworkConfigManager>(fConfigManager);
+
 
 	LOG(LogLevel::Fine) << "int main(int,char*) : Create Config Manager [MeteorConfigManager].";
 	MeteorConfigManager* mConfigManager = new MeteorConfigManager();
 	mConfigManager->Async();
-	mConfigManager->Set(MeteorSetting::FallBrightness, MTO_FLOAT(0.6f));
-	mConfigManager->Set(MeteorSetting::FallLength, 4);
-	mConfigManager->Set(MeteorSetting::WhiteKeyTargetHeight, 4);
-	mConfigManager->Set(MeteorSetting::BlackKeyTargetHeight, 2);
-	mConfigManager->Set(MeteorSetting::TargetLineBlinkSpeed, MTO_FLOAT(0.5f));
-	mConfigManager->Set(MeteorSetting::TargetLineBrightness, MTO_FLOAT(0.1f));
+	mConfigManager->Set(MeteorSetting::WhiteKeyTargetHeight	, 4);
+	mConfigManager->Set(MeteorSetting::BlackKeyTargetHeight	, 2);
+	mConfigManager->Set(MeteorSetting::FallSpeed			, MTO_FLOAT(24.f));
+	mConfigManager->Set(MeteorSetting::FallBrightness		, MTO_FLOAT(0.6f));
+	mConfigManager->Set(MeteorSetting::FallLength			, 4);
+	mConfigManager->Set(MeteorSetting::ExplodeSpeed			, MTO_FLOAT(0.6f));
+	mConfigManager->Set(MeteorSetting::ExplodeBrightness	, MTO_FLOAT(0.6f));
+	mConfigManager->Set(MeteorSetting::GlowLineSpeed		, MTO_FLOAT(0.6f));
+	mConfigManager->Set(MeteorSetting::GlowLineDuration		, MTO_FLOAT(0.6f));
+	mConfigManager->Set(MeteorSetting::GlowLineBrightness	, MTO_FLOAT(0.6f));
+	mConfigManager->Set(MeteorSetting::TargetLineBlinkSpeed	, MTO_FLOAT(0.5f));
+	mConfigManager->Set(MeteorSetting::TargetLineBrightness	, MTO_FLOAT(0.1f));
 	game->Cache<MeteorConfigManager>(mConfigManager);
+
 
 	// 1. cache session
 	// TODO: 之後把session改成我要的
